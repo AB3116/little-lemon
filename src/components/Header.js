eaa3da1bf892914ccc3331e598070b232cb68cae
Apurva <apurvabhavsar31@gmail.com>
2023-06-16 19:35:34 +0530

    header section completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import "./styles/Header.css";
-import React from "react";
 import Nav from "./Nav";
 
 const Header = () => {
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import Header from '../Header'
 
 const Homepage = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+    </>
   )
 }
 
