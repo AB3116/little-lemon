434aa20bde0094f0ef5cfdb8d4920d02bd85af5c
Apurva <apurvabhavsar31@gmail.com>
2023-06-16 22:00:24 +0530

    booking page done

--- src/components/pages/BookingPage.js
+++ src/components/pages/BookingPage.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useReducer } from "react";
+import BookingForm from "../BookingForm";
+import Header from "../Header";
+import Footer from "../Footer";
 
 const BookingPage = () => {
-  return (
-    <div>BookingPage</div>
-  )
-}
+  // const availableSlots = [
+  //   {
+  //     date: "2023-06-17",
+  //     slots: ["21:00", "22:00", "23:00"],
+  //   },
+  //   {
+  //     date: "2023-06-18",
+  //     slots: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
+  //   },
+  //   {
+  //     date: "2023-06-16",
+  //     slots: ["21:00", "22:00"],
+  //   },
+  // ];
 
-export default BookingPage
+  const updateTimes = (state, action) => {
+    switch (action.type) {
+      case "2023-06-16":
+        return ["21:00", "22:00", "23:00"];
+      case "2023-06-17":
+        return ["21:00", "22:00"];
+      case "2023-06-18":
+        return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
+      default:
+        return state;
+    }
+  };
+
+  const initializeTimes = () => {
+    return [];
+  };
+
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+
+  return (
+    <>
+      <Header />
+      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
+      <Footer />
+    </>
+  );
+};
+
+export default BookingPage;
