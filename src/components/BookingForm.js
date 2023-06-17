fe56ca7071f4a559b68dcdb30098facc750ec0c2
Apurva <apurvabhavsar31@gmail.com>
2023-06-17 14:19:00 +0530

    styled form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,8 +1,10 @@
+import "./styles/BookingForm.css";
+
 import { useState } from "react";
 
 const BookingForm = ({ availableTimes, dispatch }) => {
   const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-  const [time, setTime] = useState("18:00");
+  const [time, setTime] = useState(availableTimes[0]);
   const [guests, setGuests] = useState(4);
   const [occasion, setOccasion] = useState("birthday");
 
@@ -11,50 +13,67 @@ const BookingForm = ({ availableTimes, dispatch }) => {
     dispatch({ type: date });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
-      <h1>Book Now</h1>
-      <label htmlFor="res-date">Choose date</label>
-      <input
-        type="date"
-        id="res-date"
-        min={new Date().toISOString().split("T")[0]}
-        value={date}
-        onChange={handleDateChange}
-      />
-      <label htmlFor="res-time">Choose time</label>
-      <select
-        id="res-time"
-        value={time}
-        onChange={(event) => setTime(event.target.value)}
+    <div className="form-container">
+      <form
+        style={{ display: "grid", maxWidth: "500px", gap: "20px" }}
+        onSubmit={handleSubmit}
       >
-        {availableTimes.map((availableTime) => (
-          <option key={availableTime} value={availableTime}>
-            {availableTime}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="guests">Number of guests</label>
-      <input
-        type="number"
-        placeholder="1"
-        min="1"
-        max="10"
-        id="guests"
-        value={guests}
-        onChange={(event) => setGuests(event.target.value)}
-      />
-      <label htmlFor="occasion">Occasion</label>
-      <select
-        id="occasion"
-        value={occasion}
-        onChange={(event) => setOccasion(event.target.value)}
-      >
-        <option value="birthday">Birthday</option>
-        <option value="anniversary">Anniversary</option>
-      </select>
-      <input type="submit" value="Make Your reservation" />
-    </form>
+        <label htmlFor="res-date">Choose date</label>
+        <input
+          type="date"
+          id="res-date"
+          min={new Date().toISOString().split("T")[0]}
+          value={date}
+          onChange={handleDateChange}
+        />
+
+        <label htmlFor="res-time">Choose time</label>
+        <select
+          id="res-time"
+          value={time}
+          onChange={(event) => setTime(event.target.value)}
+        >
+          {availableTimes &&
+            availableTimes.map((availableTime) => (
+              <option key={availableTime} value={availableTime}>
+                {availableTime}
+              </option>
+            ))}
+        </select>
+
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          value={guests}
+          onChange={(event) => setGuests(event.target.value)}
+        />
+        
+        <label htmlFor="occasion">Occasion</label>
+        <select
+          id="occasion"
+          value={occasion}
+          onChange={(event) => setOccasion(event.target.value)}
+        >
+          <option value="birthday">Birthday</option>
+          <option value="anniversary">Anniversary</option>
+        </select>
+        
+        <input
+          type="submit"
+          value="Make Your Reservation"
+          className="button form-variant"
+        />
+      </form>
+    </div>
   );
 };
 
--- src/components/pages/BookingPage.js
+++ src/components/pages/BookingPage.js
@@ -1,39 +1,24 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import BookingForm from "../BookingForm";
 import Header from "../Header";
 import Footer from "../Footer";
 
 const BookingPage = () => {
-  // const availableSlots = [
-  //   {
-  //     date: "2023-06-17",
-  //     slots: ["21:00", "22:00", "23:00"],
-  //   },
-  //   {
-  //     date: "2023-06-18",
-  //     slots: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
-  //   },
-  //   {
-  //     date: "2023-06-16",
-  //     slots: ["21:00", "22:00"],
-  //   },
-  // ];
-
   const updateTimes = (state, action) => {
-    switch (action.type) {
-      case "2023-06-16":
-        return ["21:00", "22:00", "23:00"];
-      case "2023-06-17":
-        return ["21:00", "22:00"];
-      case "2023-06-18":
-        return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
-      default:
-        return state;
-    }
+    // switch (action.type) {
+    //   case "2023-06-16":
+    //     return ["21:00", "22:00", "23:00"];
+    //   case "2023-06-17":
+    //     return ["21:00", "22:00"];
+    //   case "2023-06-18":
+    //     return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
+    //   default:
+    return state;
+    // }
   };
 
   const initializeTimes = () => {
-    return [];
+    return ["17:00", "18:00"];
   };
 
   const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
