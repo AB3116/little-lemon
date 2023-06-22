b8b91a0fa5dd5eef775e514848cf6db71154e1e1
Apurva <apurvabhavsar31@gmail.com>
2023-06-22 21:40:34 +0530

    solved routing error

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./components/pages/HomePage";
 import BookingPage from "./components/pages/BookingPage";
-import ConfirmedBooking from "./components/pages/ConfirmedBooking";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/booking" element={<BookingPage />}></Route>
-      <Route path="/confirmedbooking" element={<ConfirmedBooking />}></Route>
     </Routes>
   );
 }
