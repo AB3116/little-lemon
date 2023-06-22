29b9bc77d1c1af54484827b25ebff25580942f37
Apurva <apurvabhavsar31@gmail.com>
2023-06-22 14:20:47 +0530

    tested clicking behavior

--- src/components/__tests__/Formpage.test.js
+++ src/components/__tests__/Formpage.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import { FormDataContext } from "../Form/FormContext";
 import FormikForm from "../FormikForm";
 
@@ -26,5 +26,35 @@ describe("FormikForm", () => {
     expect(getByLabelText(/Credit Card Number/)).toBeInTheDocument();
     expect(getByLabelText(/Expiration Date/)).toBeInTheDocument();
     expect(getByLabelText(/CVV\/CVC/)).toBeInTheDocument();
+  });
+
+  it("should show 'Required' message when input is clicked and then clicked outside", async () => {
+    const formData = {
+      date: "2023-06-22",
+      time: "17:00",
+      number: "4",
+      occasion: "birthday",
+    };
+
+    const { getByLabelText, getByText, queryByText } = render(
+      <FormDataContext.Provider value={{ formData }}>
+        <FormikForm />
+      </FormDataContext.Provider>
+    );
+
+    // Simulate clicking on the 'First name' input
+    const firstNameInput = getByLabelText(/First name/);
+    fireEvent.focus(firstNameInput);
+
+    // Assert that 'Required' message is not yet displayed
+    expect(queryByText(/Required/)).toBeNull();
+
+    // Simulate clicking outside the input
+    fireEvent.blur(firstNameInput);
+
+    // Wait for form validation to complete
+    await waitFor(() => {
+      expect(getByText(/Required/)).toBeInTheDocument();
+    });
   });
 });
